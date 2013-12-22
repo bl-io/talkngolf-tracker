bd9530670fa3fca89196d6ceee49c3f4b505cc89
Dwayne Ford <dwayneford@gmail.com>
2013-12-22 15:14:20 -0500

    Updated app-loader files

--- app/app.js
+++ app/app.js
@@ -2,23 +2,85 @@ var app = angular.module('TnG', ['ngRoute'])
 
     .value('endpoint', 'https://talkngolf.firebaseio.com')
 
-    .factory('Talkngolf', function ($firebase, endpoint) {
-        return $firebase(new Firebase(endpoint));
-    })
-
     .config(function ($routeProvider) {
         $routeProvider
-            .when('/choose', {
+
+            .when('/', {
                 controller:'Chooser',
                 templateUrl:'app/views/chooser.angv'
             })
+
+            .when('/play/choose/clubset', {
+                controller:'Clubset',
+                templateUrl:'app/views/choose_clubset.angv'
+            })
+
+            .when('/play/choose/course', {
+                controller:'Course',
+                templateUrl:'app/views/choose_course.angv'
+            })
     })
 
-    .controller('Chooser', function ($scope) {
+    .service('PlaySetup', function ($http, $location) {
+        var endpoint = "https://talkngolf.firebaseio.com/";
+
+        this.getPanes = function (collection, callback) {
+            $http({method: 'GET', url: endpoint + collection + '/.json'})
+                .success(function (data, status, headers, config) {
+                    callback(data);
+                })
+        };
+
+        this.continuePlaySetup = function (nextStep, dataToSave) {
+            $location.path(nextStep);
+        };
+
+        this.registerData = function (collection, saveToModel) {
+            $http({method: 'GET', url: endpoint + collection + '/.json'})
+                .success(function (data, status, headers, config) {
+                    saveToModel(data);
+                })
+        };
+    })
+
+    .controller('Round', function ($scope) {
+        $scope.$on('round-update', function() {
+            alert('round updated');
+        });
+    })
+
+    .controller('Chooser', function ($scope, PlaySetup) {
+        $scope.continuePlaySetup = function (nextStep) {
+            PlaySetup.continuePlaySetup(nextStep);
+        }
         $scope.heading = "Choose";
         $scope.subHeading = "Starting hole";
         $scope.panes = {
-                "Practice": "/practice",
+                "Practice": "#",
                 "Play": "/play/choose/clubset"
             }
-    });
+    })
+
+    .controller('Clubset', function ($scope, PlaySetup) {
+        var baseCollection = collection = 'clubs';
+
+        $scope.continuePlaySetup = function (clubData) {
+            collection = baseCollection + "/" + clubData;
+            PlaySetup.registerData(collection, function(retrievedData) {
+                $scope.$emit('round-update');
+            });
+        };
+
+        $scope.heading = "Choose Club Set";
+        PlaySetup.getPanes(collection, function (retrievedClubset) {
+            $scope.panes = retrievedClubset;
+        });
+    })
+
+    .controller('Course', function ($scope, PlaySetup) {
+        $scope.continuePlaySetup = function (nextStep) {
+            PlaySetup.continuePlaySetup(nextStep);
+        }
+        $scope.heading = "Choose Club Set";
+        $scope.panes = {}
+    })
