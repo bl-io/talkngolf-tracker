0357ac8686884c3484a564535a365931004ea6d1
Dwayne Ford <dwayneford@gmail.com>
2013-12-26 10:35:39 -0500

    WIP saves shot data to database but not in the preferred format

--- app/services/play_tracking.js
+++ app/services/play_tracking.js
@@ -1,8 +1,10 @@
 angular.module('TnG')
 
 .service('PlayTracking', function ($http, $location, $route, $routeParams) {
-        this.mulliganLog = [];
+        this.mulliganLog = [],
+        this.shotLog = {};
         this.endpoint = "https://talkngolf.firebaseio.com/";
+        this.roundEndpoint = "https://tng-rounds.firebaseio.com";
 
         this.fetch = function (collection, callback) {
 
@@ -24,20 +26,30 @@ angular.module('TnG')
             }
         };
 
+        this.saveToMulliganLog = function (dataToSave) {
+            this.mulliganLog.push(dataToSave);
+        }
+
         this.mulligan = function () {
             var dataToSave = {}, currentHole, currentShot, selectedClub, moodSwing, nextStep, params;
             params = $routeParams;
 
-            this.mulliganLog.push(params);
+            for(paramName in params) {
+                dataToSave[paramName] = params[paramName];
+            }
+
+            this.saveToMulliganLog(dataToSave);
+
+            console.log(this.mulliganLog);
 
             var doOverLocation = $location.path().split('/');
+
             doOverLocation.pop(); //remove the moodSwing
             doOverLocation.pop(); //remove the club selection
 
             nextStep = doOverLocation.join('/');
-
-            this.continue(nextStep);
             console.log('mulligan shot');
+            this.continue(nextStep);
         };
 
         this.editTracking = function () {
@@ -49,6 +61,54 @@ angular.module('TnG')
         };
 
         this.nextShot = function () {
-            console.log('next shot');
+            var params = $routeParams,
+                baseUrl,
+                dataToSave = {},
+                currentShotAsNumber,
+                newParams = [],
+                nextShotAsNumber;
+
+
+            for(paramName in params) {
+                if (paramName == "selectedClub" || paramName == "moodSwing" || paramName == "currentHole") {
+                    dataToSave[paramName] = params[paramName];
+                }
+            }
+
+            dataToSave.time = Date();
+
+//            if(!this.shotLog[params.currentHole]) {
+//                this.shotLog[params.currentHole] = [];
+//            };
+
+            if(!this.shotLog["1"]) {
+                this.shotLog["1"] = [];
+            };
+
+            this.shotLog[params.currentHole].push(dataToSave);
+
+            $http.put(this.roundEndpoint + "/.json", this.shotLog);
+
+            currentShotAsNumber = parseInt(params.currentShot);
+
+            nextShotAsNumber = currentShotAsNumber + 1;
+
+            params.currentShot = nextShotAsNumber.toString();
+
+            baseUrl = $location.path().split('/');
+            baseUrl.pop();
+            baseUrl.pop();
+            baseUrl.pop();
+            baseUrl.pop();
+
+            for(paramName in params) {
+
+                if(paramName == "currentHole" || paramName == "currentShot") {
+                    newParams.push(params[paramName]);
+                }
+            }
+            nextStep = baseUrl.join('/') + "/" + newParams.join('/');
+
+            this.continue(nextStep);
         };
 })
